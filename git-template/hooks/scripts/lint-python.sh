f9154e871b5b7fa1da1bd33c6f42f5fac7e2ac69
h.tsutsumi <h.tsutsumi1203@gmail.com>
2017-03-15 12:21:11 +0900

    ファイルの存在チェックをしてから構文チェックをする

--- git-template/hooks/scripts/lint-python.sh
+++ git-template/hooks/scripts/lint-python.sh
@@ -4,10 +4,12 @@ IS_ERROR=0
 
 #.py構文チェック
 for FILE in `git diff-index --name-status HEAD | grep -E '*\.py$' | cut -c3-`; do
-    if pyflakes $FILE; then
-        :
-    else
-        IS_ERROR=1
+    if [ -e $FILE ]; then
+        if pyflakes $FILE; then
+            :
+        else
+            IS_ERROR=1
+        fi
     fi
 done
 
